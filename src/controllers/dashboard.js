5054f2bc78213f6990e114616ae9468b61c401bd
Nico Picchio <nicolapicchio@gmail.com>
2022-07-04 16:40:16 +0100

    corrected automated formatting

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -12,21 +12,21 @@ export const getData = async (req, res) => {
 		};
 		res.status(200).send({ userData });
 	} catch (err) {
-    res.status(404).send('Not found');
+		res.status(404).send('Not found');
 	}
 };
 
 export const addData = async (req, res) => {
 	const userId = req.body.userId;
-  try {
-    const userFound = await User.findById(userId);
-    userFound.sleep.push(parseInt(req.body.sleep)),
-    userFound.weight.push(parseInt(req.body.weight)),
-    userFound.fatigue.push(parseInt(req.body.fatigue)),
-    userFound.restHR.push(parseInt(req.body.restHR))
-    userFound.save()
-    res.status(200).send(userFound)
-  } catch (err) {
-    res.status(404).send('Not found')
-  }
+	try {
+		const userFound = await User.findById(userId);
+		userFound.sleep.push(parseInt(req.body.sleep)),
+		userFound.weight.push(parseInt(req.body.weight)),
+		userFound.fatigue.push(parseInt(req.body.fatigue)),
+		userFound.restHR.push(parseInt(req.body.restHR));
+		userFound.save();
+		res.status(200).send(userFound);
+	} catch (err) {
+		res.status(404).send('Not found');
+	}
 };
